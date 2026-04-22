71e7eac7f877315695ca3258b02bfe33908904a5
= <=>
2026-04-22 22:48:21 +0700

    Pengerjaan Geomatry

--- JajarGenjang.js
+++ JajarGenjang.js
@@ -1,11 +1,10 @@
 const prompt=require('prompt-sync')({sigint: true})
 let alas = Number(prompt("Masukkan nilai Alas: "))
 let tinggi = Number(prompt("Masukkan nilai Tinggi: "))
-let a = Number(prompt("Masukkan nilai A: "))
-let b = Number(prompt("Masukkan nilai B: "))
+let b = Number(prompt("Masukkan nilai sisi miring: "))
 
 let luas = alas * tinggi
-let keliling = 2 * (a + b)
+let keliling = 2 * (alas + b)
 
 console.log("---------------")
 console.log("      HASIL     ")
